f650cad97d2faee159a0b693dc35346d5e789a89
zhou-tao <zhoutao_233@163.com>
2023-08-18 09:28:39 +0800

    docs: update usage

--- examples/vite-vue3/vite.config.ts
+++ examples/vite-vue3/vite.config.ts
@@ -3,17 +3,18 @@ import vue from '@vitejs/plugin-vue'
 import envParser, { parse } from 'vite-plugin-env-parser'
 
 export default defineConfig(({ mode }) => {
-  const a: EnvTypes = {}
+  const root = process.cwd()
   const envPrefix = ['VITE_', 'APP_']
-  const { VITE_PORT } = parse(loadEnv(mode, process.cwd(), envPrefix))
+  const { VITE_PORT } = parse(loadEnv(mode, root, envPrefix))
   return {
+    root,
+    envPrefix,
     plugins: [
       vue(),
       envParser({
         dts: true
       })
     ],
-    envPrefix,
     server: {
       port: VITE_PORT
     }
